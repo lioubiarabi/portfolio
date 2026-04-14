cf5c1e1088a00baa97328f8f75cdd506db02a5a2
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-04-14 11:30:00 +0100

    setup responsive nav

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const navbar = document.getElementById('navbar');
+if(navbar) {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+}
